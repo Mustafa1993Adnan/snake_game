ba02553ab6c3590e6a5cdb0d18f1cc8c3d93f59d
Mustafa Adnan <Mustafa1993tofi@gmail.com>
2021-06-05 18:25:33 +0300

    Changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ screen = Screen()
 
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
-screen.title('Snake Game')
+screen.title('Snake Game 123')
 
 segments = [(0, 0), (-20, 0), (-40, 0)]
 for position in segments:
